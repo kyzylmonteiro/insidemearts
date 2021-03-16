ec688a4c5ec9c10f36a488f32ddb4015c203edfb
kyzylmonteiro <32486621+kyzylmonteiro@users.noreply.github.com>
2021-03-16 17:56:02 +0530

    links corrected probably pt 2

--- src/components/Header.js
+++ src/components/Header.js
@@ -10,43 +10,28 @@ const Header = () => {
         <Navbar.Collapse id="responsive-navbar-nav">
           <Nav className="mx-auto parent">
             <Nav.Item>
-              <Nav.Link
-                className="navLink"
-                href="/"
-              >
-                Inside Me
+              <Nav.Link>
+                <Link className="navLink" activeClassName="activeNavLink" to="/">Inside Me</Link>
               </Nav.Link>
             </Nav.Item>
             <Nav.Item>
-              <Nav.Link
-                className="navLink"
-                href="/initiative"
-              >
-                Our Initiative
+              <Nav.Link>
+                <Link className="navLink" activeClassName="activeNavLink" to="/initiative">Our Initiative</Link>
               </Nav.Link>
             </Nav.Item>
             <Nav.Item>
-              <Nav.Link
-                className="navLink"
-                href="/media"
-              >
-                Media
+              <Nav.Link>
+                <Link className="navLink" activeClassName="activeNavLink" to="/media">Media</Link>
               </Nav.Link>
             </Nav.Item>
             <Nav.Item>
-              <Nav.Link
-                className="navLink"
-                href="/donate"
-              >
-                Donate
+              <Nav.Link>
+                <Link className="navLink" activeClassName="activeNavLink" to="/donate">Donate</Link>
               </Nav.Link>
             </Nav.Item>
             <Nav.Item>
-              <Nav.Link
-                className="navLink"
-                href="/contact"
-              >
-                Contact Us
+              <Nav.Link>
+                <Link className="navLink" activeClassName="activeNavLink" to="/contact">Contact Us</Link>
               </Nav.Link>
             </Nav.Item>
             {/* <Nav.Item>
